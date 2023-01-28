720a5d00676ab694388510c455ba12b66fe6e433
pingopengo <github@pvernau.de>
2023-01-28 15:49:23 +0100

    CLI Pomodoro Timer. Adding functionality as we go

---
+++ test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	duration              = 25
+	shortBreak            = 5
+	longBreak             = 15
+	pomodoros             int
+	pomodoroSelectionDone bool
+)
+
+func main() {
+	for {
+		if !pomodoroSelectionDone {
+			pomodoroSelection()
+
+		} else if pomodoroSelectionDone == true {
+
+			fmt.Println("1. Start timer with default settings! 😊")
+			fmt.Println("2. Start timer with custom settings! 🤓")
+			fmt.Println("3. Exit! 😢")
+
+			var input int
+			fmt.Print("Enter your choice: ")
+			fmt.Scan(&input)
+
+			switch input {
+			case 1:
+				startTimer(duration, shortBreak, longBreak)
+
+			case 2:
+				setCustomTimer()
+				startTimer(duration, shortBreak, longBreak)
+
+			case 3:
+				fmt.Println("die stupid! 😡")
+				return
+			default:
+				fmt.Println("Invalid input, try again 😤")
+			}
+		}
+	}
+}
+
+func pomodoroSelection() {
+	fmt.Println("Welcome to the Pomodoro Timer!")
+	fmt.Println("How many pomodoros would you like to do?")
+	fmt.Scan(&pomodoros)
+	pomodoroSelectionDone = true
+}
+
+func setCustomTimer() {
+	fmt.Println("Enter the duration of the pomodoro in minutes:")
+	fmt.Scan(&duration)
+	fmt.Println("Enter the duration of the short break in minutes:")
+	fmt.Scan(&shortBreak)
+	fmt.Println("Enter the duration of the long break in minutes:")
+	fmt.Scan(&longBreak)
+}
+
+func startTimer(duration, shortBreak, longBreak int) {
+	var pomodorosCompleted int
+	for pomodorosCompleted < pomodoros {
+		if duration == 1 {
+			fmt.Println("Starting timer for", duration, "minute! 🕑")
+		} else {
+			fmt.Println("Starting timer for", duration, "minutes! 🕑")
+		}
+
+		time.Sleep(time.Duration(duration) * time.Minute)
+		fmt.Println("Timer ended! 🕑")
+		pomodorosCompleted++
+
+		if pomodorosCompleted%4 == 0 {
+			fmt.Println("Starting timer for", longBreak, "minutes! 🕑")
+			time.Sleep(time.Duration(longBreak) * time.Minute)
+			fmt.Println("Timer ended! 🕑")
+		} else if pomodorosCompleted == pomodoros {
+			fmt.Println("You have completed", pomodoros, "pomodoros! 🎉")
+			break
+		} else {
+			fmt.Println("Starting timer for", shortBreak, "minutes! 🕑")
+			time.Sleep(time.Duration(shortBreak) * time.Minute)
+			fmt.Println("Timer ended! 🕑")
+		}
+
+	}
+
+	fmt.Println("Timer started! 🕑")
+	fmt.Println("You have", duration, "minutes to work! 📝")
+	fmt.Println("You have", shortBreak, "minutes for a short break! ☕️")
+	fmt.Println("You have", longBreak, "minutes for a long break! 🍃")
+}
